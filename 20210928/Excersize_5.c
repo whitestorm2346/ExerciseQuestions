c93cce0741a654f21762388b48c129ed2a477e0b
whitestorm2346 <andrew770426@gmail.com>
2021-11-01 09:34:50 +0800

    Added a new program (c)

---
+++ 20210928/Excersize_5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int x1;
+    float x2;
+    char x3;
+    char x4[20];
+
+    scanf("%d %f %c %s", &x1, &x2, &x3, x4);
+    printf("%d %f %c %s", x1, x2, x3, x4);
+
+	return 0;
+}
