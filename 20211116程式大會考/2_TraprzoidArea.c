5b5cb869668621048400b6699dd6062ab24916b5
whitestorm2346 <andrew770426@gmail.com>
2021-11-16 17:57:58 +0800

    Added a test code (c)

---
+++ 20211116程式大會考/2_TraprzoidArea.c
@@ -0,0 +1,12 @@
+// ID:410411218 name:¥ÂÃ ≥‘ class:1A
+#include <stdio.h>
+
+int main()
+{
+    int int_Top, int_Bottom, int_Height;
+
+    scanf("%d %d %d", &int_Top, &int_Bottom, &int_Height);
+    printf("%f", (int_Top + int_Bottom) * int_Height / 2.0);
+
+    return 0;
+}
