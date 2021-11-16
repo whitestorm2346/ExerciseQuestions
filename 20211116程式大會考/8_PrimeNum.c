f9d039a25cff2eaf979b5f6e13506da2d55200d9
whitestorm2346 <andrew770426@gmail.com>
2021-11-16 17:58:58 +0800

    Added a test code (c)

---
+++ 20211116程式大會考/8_PrimeNum.c
@@ -0,0 +1,29 @@
+// ID:410411218 name:¥ÂÃ ≥‘ class:1A
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    int int_Num;
+
+    scanf("%d", &int_Num);
+
+    for(int i = 2; i <= int_Num; i++)
+    {
+        _Bool bl_IsPrimeNum = 1;
+
+        for(int j = 2; j <= sqrt(i); j++)
+        {
+            if(i % j == 0)
+            {
+                bl_IsPrimeNum = 0;
+
+                break;
+            }
+        }
+
+        if(bl_IsPrimeNum) printf("%d ", i);
+    }
+
+    return 0;
+}
