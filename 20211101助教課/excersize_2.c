630fa775fc3206d985616b722655750e982aea1c
whitestorm2346 <andrew770426@gmail.com>
2021-12-06 10:18:04 +0800

    Added a new program (c)

---
+++ 20211101助教課/excersize_2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int intarr_Num[5] = {2, 5};
+
+    for(int i = 0; i < 5; i++)
+    {
+        printf("arr[%d] -> %d\n", i, intarr_Num[i]);
+    }
+
+    return 0;
+}
