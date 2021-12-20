7860ca29a85ad3e648d5a35a2e7b27a764f77797
whitestorm2346 <andrew770426@gmail.com>
2021-12-20 10:53:56 +0800

    Modified the code

--- 20211206助教課/Hanoi.c
+++ 20211206助教課/Hanoi.c
@@ -18,8 +18,8 @@ void Hanoi(int int_Plates, int int_Begin, int int_Mid, int int_Des)
     if(int_Plates == 1) printf("Plate%d: %d->%d\n", int_Plates, int_Begin, int_Des);
     else
     {
-        Hanoi(int_Plates - 1, int_Begin, int_Des, int_Mid);
+        Hanoi(int_Plates - 1, int_Begin, int_Des, int_Mid); // move second~int_Plates
         printf("Plate%d: %d->%d\n", int_Plates, int_Begin, int_Des);
-        Hanoi(int_Plates - 1, int_Mid, int_Begin, int_Des);
+        Hanoi(int_Plates - 1, int_Mid, int_Begin, int_Des); // move top
     }
 }
