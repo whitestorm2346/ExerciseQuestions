4b2ca3ddfd3ac6a59520ed8a721472da1db5628f
whitestorm2346 <andrew770426@gmail.com>
2021-12-27 11:33:53 +0800

    Added a test code (c)

---
+++ 20211227助教課/410411218_4.c
@@ -0,0 +1,33 @@
+// name: Yu, Tse-Hsun class: 1A ID: 410411218
+#include <stdio.h>
+
+void primeNum(int num, int curr)
+{
+    if(num == 0 || num == 1) return;
+
+    if(num % curr == 0)
+    {
+        printf("%d", curr);
+
+        num /= curr;
+
+        if(num != 0 && num != 1)
+        {
+            printf("*");
+        }
+
+        primeNum(num, curr);
+    }
+    else primeNum(num, curr + 1);
+}
+
+int main()
+{
+    int num;
+
+    scanf("%d", &num);
+
+    primeNum(num, 2);
+
+    return 0;
+}
